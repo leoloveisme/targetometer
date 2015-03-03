e1be647ab9686e8ef973731d786b830e8c42fc7a
leozukov <lad@lad-530U3BI-530U4BI-530U4BH.(none)>
2015-03-03 14:27:01 +0530

    first uploading 3 march 2015 14:26 indian time

---
+++ index.php
@@ -0,0 +1,213 @@
+<table width=100%>
+<tr>
+<td align=right>Hello, <b>guest</b>. <a href="">Register</a> <a href="">Login</a> </td>
+</tr>
+</table>
+<table width=100%>
+<tr>
+<td width=10%></td>
+<td width=90%>
+<b>Enter your goal | task | target</b>
+<br><br>
+<textarea></textarea> 
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">Today</OPTION>
+<OPTION>This week</OPTION>
+<OPTION>This month</OPTION>
+<OPTION>This year</OPTION>
+<OPTION>Other day</OPTION>
+<OPTION>Other week</OPTION>
+<OPTION>Other month</OPTION>
+<OPTION>Other year</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">date</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">hours</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">minutes</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">work week number</OPTION>
+<OPTION>1</OPTION>
+<OPTION>15</OPTION>
+<OPTION>23</OPTION>
+<OPTION>36</OPTION>
+<OPTION>47</OPTION>
+<OPTION>52</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">month</OPTION>
+<OPTION>January</OPTION>
+<OPTION>February</OPTION>
+<OPTION>March</OPTION>
+<OPTION>April</OPTION>
+<OPTION>May</OPTION>
+<OPTION>December</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">year</OPTION>
+<OPTION>2015</OPTION>
+<OPTION>2016</OPTION>
+<OPTION>2018</OPTION>
+<OPTION>2025</OPTION>
+<OPTION>2050</OPTION>
+<OPTION>2121</OPTION>
+</SELECT>
+* (date and time attributes aren't compulsary)
+<br><br>
+<button>Save</button>
+</td>
+</tr>
+</table>
+<br><hr width=100%>
+<br><br>
+<table width=100%>
+<tr>
+<td width=5%></td>
+<td width=95%>
+<b>Goals</b> <SELECT NAME="">
+<OPTION SELECTED VALUE="">Today</OPTION>
+<OPTION>This week</OPTION>
+<OPTION>This month</OPTION>
+<OPTION>This year</OPTION>
+<OPTION>Other day</OPTION>
+<OPTION>Other week</OPTION>
+<OPTION>Other month</OPTION>
+<OPTION>Other year</OPTION>
+<OPTION>Completed</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">date</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">hours</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">minutes</OPTION>
+<OPTION>1</OPTION>
+<OPTION>2</OPTION>
+<OPTION>3</OPTION>
+<OPTION>28</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">work week number</OPTION>
+<OPTION>1</OPTION>
+<OPTION>15</OPTION>
+<OPTION>23</OPTION>
+<OPTION>36</OPTION>
+<OPTION>47</OPTION>
+<OPTION>52</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">month</OPTION>
+<OPTION>January</OPTION>
+<OPTION>February</OPTION>
+<OPTION>March</OPTION>
+<OPTION>April</OPTION>
+<OPTION>May</OPTION>
+<OPTION>December</OPTION>
+</SELECT>
+<SELECT NAME="">
+<OPTION SELECTED VALUE="">year</OPTION>
+<OPTION>2015</OPTION>
+<OPTION>2016</OPTION>
+<OPTION>2018</OPTION>
+<OPTION>2025</OPTION>
+<OPTION>2050</OPTION>
+<OPTION>2121</OPTION>
+</SELECT>
+<br><br>
+1. Meet up with Mr. X <a href="">Edit</a> <a href="">Complete | Archive</a>
+<br><br>
+<span style="background-color: blue; color:white; font-weight:bold">business;</span> <span style="background-color: blue; color:white; font-weight:bold">businessman;</span> <span style="background-color: red; color:white; font-weight:bold">important, urgent;</span>
+<br><br>
+Note:
+<br><br>
+<textarea></textarea>
+<br><br>
+<button>Save</button>
+<br><br>
+2. <textarea>Buy an apartment in Barcelona, Spain by 1 July 2015</textarea> <a href="">Save</a> <a href="">Complete | Archive</a>
+<br><br>
+<span style="background-color: orange; color:white; font-weight:bold">personal;</span> <span style="background-color: red; color:white; font-weight:bold">money;</span> <span style="background-color: yellow; color:green; font-weight:bold">important,  not urgent;</span>
+<br><br>
+Note:
+<br><br>
+<textarea></textarea>
+<br><br>
+<button>Save</button>
+<br><br>
+3. Publish my book <a href="">Edit</a> <a href="">Complete | Archive</a>
+<br><br>
+<span style="background-color: blue; color:white; font-weight:bold">business;</span> <span style="background-color: blue; color:white; font-weight:bold">businessman;</span> <span style="background-color: red; color:white; font-weight:bold">important, urgent;</span>
+<br><br>
+Note:
+<br><br>
+<textarea>Currently negotiating terms with editor</textarea>
+<br><br>
+<button>Save</button>
+</td>
+</tr>
+</table>
+<br><hr width=100%>
+<br><br>
+<table width=100%>
+<tr>
+<td width=5%></td>
+<td width=95%>
+<b>Drag and dropable calendar</b> (where you can drop your targets into) <a href="">Expand</a>
+<br><br>
+<hr width=100%>
+<b>Sorting tasks quadrant</b> <a href="">Expand</a>
+<br><br>
+<hr width=100%>
+<b>GTD</b> <a href="">Expand</a>
+<br><br>
+<hr width=100%>
+<b>Roles tasks</b>  <a href="">Collapse</a>
+<br><br>
+<input value="Son"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Father"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Uncle"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Friend"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Brother"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Business owner"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<input value="Husband"> <a href="">Edit</a> <a href="">Delete</a>
+<br><br>
+<a href="">Add role</a>
+<br><br>
+<hr width=100%>
+<a href="">Add other method, indicator or tool</a>
+<br><br>
+</td>
+</tr>
+</table>
