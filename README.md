0fc0108cbb6ea5d36d219eac26bdef1959211461
leozukov <leozukov@gmail.com>
2016-08-24 13:06:18 +0200

    Did some changes to the README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # targetometer
+
+An app and a platform for sorting, prioritising and colaborating on tasks
+
+Chat
+Profile
+
+and more
--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <html>
 <head>
-	<title></title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-    $("a").click(function(){
-        $("span").toggle();
-    });
-});
-</script>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+		<title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	</head>
 </head>
 <body>
 <p><a name="top"></a> <?php include ("header.php"); ?></p>
